311bb810de0e9c54054fe4cc6582419d1c300414
Ahmed_Elzatry <Ahmedelzatry2000@gmail.com>
2025-05-15 15:22:59 +0200

    refactor

--- OddEvenNumbersApp/Program.cs
+++ OddEvenNumbersApp/Program.cs
@@ -5,6 +5,8 @@ int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 PrintNumbers("IS EVEN", numbers.Where(x => x % 2 == 0));
 PrintNumbers("IS ODD", numbers.Where(x => x % 2 != 0));
 
+Console.WriteLine("\n\n\t\tREFACTOR");
+
 static void PrintNumbers (string title,IEnumerable<int> numbers)
 {
     Console.WriteLine();
