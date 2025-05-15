277db08329211d7ccd4c4a08ab8c25179ea9d693
Ahmed_Elzatry <Ahmedelzatry2000@gmail.com>
2025-05-15 15:13:38 +0200

    initial commit

---
+++ OddEvenNumbersApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+PrintNumbers("IS EVEN", numbers.Where(x => x % 2 == 0));
+PrintNumbers("IS ODD", numbers.Where(x => x % 2 != 0));
+
+static void PrintNumbers (string title,IEnumerable<int> numbers)
+{
+    Console.WriteLine();
+    Console.Write($"{title}: [");
+    foreach (int number in numbers)
+    {  Console.Write($" {number}"); }
+    Console.Write(" ]");
+    Console.WriteLine();
+}
